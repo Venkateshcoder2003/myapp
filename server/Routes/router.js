71412bd5a13b2d477e7934df0289d092ff486258
Venkateshcoder2003 <145249783+Venkateshcoder2003@users.noreply.github.com>
2024-07-30 22:14:49 +0530

    Update router.js

--- server/Routes/router.js
+++ server/Routes/router.js
@@ -2,10 +2,89 @@
 
 
 
+// const express = require("express");
+// const router = new express.Router();
+// const users = require("../models/userSchema");
+// const nodemailer = require("nodemailer");
+
+// // email config
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: process.env.EMAIL,
+//         pass: process.env.PASS
+//     }
+// });
+
+
+// //register user details
+// router.post("/register", async (req, res) => {
+//     const { fname, lname, email, mobile, message } = req.body;
+
+//     if (!fname || !lname || !email || !mobile) {
+//         res.status(401).json({ status: 401, error: "All Input require" })
+//     }
+
+//     try {
+//         const preuser = await users.findOne({ email: email });
+
+//         if (preuser) {
+//             const userMessage = await preuser.Messagesave(message);
+//             console.log(userMessage);
+//             const mailOptions = {
+//                 from: process.env.EMAIL,
+//                 to: email,
+//                 subject: "sending email using nodejs",
+//                 text: "Your Response Has Been Submitted"
+//             }
+
+//             transporter.sendMail(mailOptions, (error, info) => {
+//                 if (error) {
+//                     console.log("error" + error)
+//                 } else {
+//                     console.log("Email sent" + info.response);
+//                     res.status(201).json({ status: 201, message: "Email sent SUccesfully" })
+//                 }
+//             });
+//         } else {
+//             const finalUser = new users({
+//                 fname, lname, email, mobile, messages: { message: message }
+//             });
+
+//             const storeData = await finalUser.save();
+
+//             const mailOptions = {
+//                 from: process.env.EMAIL,
+//                 to: email,
+//                 subject: "sending email using nodejs",
+//                 text: "Your Response Has Been Submitted"
+//             }
+
+//             transporter.sendMail(mailOptions, (error, info) => {
+//                 if (error) {
+//                     console.log("error" + error)
+//                 } else {
+//                     console.log("Email sent" + info.response);
+//                     res.status(201).json({ status: 201, message: "Email sent SUccesfully" })
+//                 }
+//             });
+//             res.status(201).json({ status: 201, storeData })
+//         }
+
+//     } catch (error) {
+//         res.status(401).json({ status: 401, error: "All Input require" });
+//         console.log("catch error")
+//     }
+
+// })
 const express = require("express");
 const router = new express.Router();
 const users = require("../models/userSchema");
 const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+// Use CORS middleware
+router.use(cors());
 
 // email config
 const transporter = nodemailer.createTransport({
@@ -16,13 +95,12 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-
 //register user details
 router.post("/register", async (req, res) => {
-    const { fname, lname, email, mobile, message } = req.body;
+    const { fname, lname, email, password, mobile, message } = req.body;
 
-    if (!fname || !lname || !email || !mobile) {
-        res.status(401).json({ status: 401, error: "All Input require" })
+    if (!fname || !lname || !email || !password || !mobile) {
+        return res.status(401).json({ status: 401, error: "All Input required" });
     }
 
     try {
@@ -34,21 +112,21 @@ router.post("/register", async (req, res) => {
             const mailOptions = {
                 from: process.env.EMAIL,
                 to: email,
-                subject: "sending email using nodejs",
+                subject: "Sending email using Node.js",
                 text: "Your Response Has Been Submitted"
-            }
+            };
 
             transporter.sendMail(mailOptions, (error, info) => {
                 if (error) {
-                    console.log("error" + error)
+                    console.log("error" + error);
                 } else {
-                    console.log("Email sent" + info.response);
-                    res.status(201).json({ status: 201, message: "Email sent SUccesfully" })
+                    console.log("Email sent: " + info.response);
+                    return res.status(201).json({ status: 201, message: "Email sent successfully" });
                 }
             });
         } else {
             const finalUser = new users({
-                fname, lname, email, mobile, messages: { message: message }
+                fname, lname, email, password, mobile, messages: { message: message }
             });
 
             const storeData = await finalUser.save();
@@ -56,27 +134,27 @@ router.post("/register", async (req, res) => {
             const mailOptions = {
                 from: process.env.EMAIL,
                 to: email,
-                subject: "sending email using nodejs",
+                subject: "Sending email using Node.js",
                 text: "Your Response Has Been Submitted"
-            }
+            };
 
             transporter.sendMail(mailOptions, (error, info) => {
                 if (error) {
-                    console.log("error" + error)
+                    console.log("error" + error);
                 } else {
-                    console.log("Email sent" + info.response);
-                    res.status(201).json({ status: 201, message: "Email sent SUccesfully" })
+                    console.log("Email sent: " + info.response);
+                    return res.status(201).json({ status: 201, message: "Email sent successfully", storeData });
                 }
             });
-            res.status(201).json({ status: 201, storeData })
         }
 
     } catch (error) {
-        res.status(401).json({ status: 401, error: "All Input require" });
-        console.log("catch error")
+        res.status(401).json({ status: 401, error: "All Input required" });
+        console.log("catch error", error);
     }
+});
 
-})
+module.exports = router;
 
 
 
