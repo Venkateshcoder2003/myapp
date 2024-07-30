8e27cb8544a48018b16543626ac5d88816667f8d
Venkateshcoder2003 <145249783+Venkateshcoder2003@users.noreply.github.com>
2024-07-30 22:15:33 +0530

    Update userSchema.js

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,5 +1,57 @@
+// const mongoose = require('mongoose');
+// const validator = require('validator');
+// const userSchema = new mongoose.Schema({
+//     fname: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     lname: {
+//         type: String,
+//         //required: true,
+//         trim: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//         validate(value) {
+//             if (!validator.isEmail(value)) {
+//                 throw new Error("invalid email")
+//             }
+//         }
+//     },
+//     mobile: {
+//         type: String,
+//         required: true,
+//         //unique: true,
+//     },
+//     messages: []
+// })
+
+// //save message
+// userSchema.methods.Messagesave = async function (message) {
+//     try {
+//         this.messages = this.messages.concat({ message });
+//         await this.save();
+//         return message;
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// //createing model
+// const users = new mongoose.model("users", userSchema);
+
+
+// module.exports = users;
+
+
+
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs")
 const validator = require('validator');
+const jwt = require('jsonwebtoken')
+
 const userSchema = new mongoose.Schema({
     fname: {
         type: String,
@@ -21,6 +73,11 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
+    password: {
+        type: String,
+        required: true,
+        unique: true
+    },
     mobile: {
         type: String,
         required: true,
@@ -28,6 +85,23 @@ const userSchema = new mongoose.Schema({
     },
     messages: []
 })
+
+
+//hashing password
+userSchema.pre("save", async function (next) {
+    const user = this;
+    if (!this.isModified("password")) {
+        next();
+    }
+    try {
+        const hashPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashPassword;
+    } catch (error) {
+        next(error)
+    }
+    console.log(this);
+});
+//JWt Authentication
 
 //save message
 userSchema.methods.Messagesave = async function (message) {
@@ -39,8 +113,25 @@ userSchema.methods.Messagesave = async function (message) {
         console.log(error)
     }
 }
+
+//authenticating user
+// userSchema.methods.generateAuthToken = async function () {
+//     try {
+//         const user = this;
+//         const token = jwt.sign(
+//             { _id: user._id.toString() },
+//             process.env.JWT_SECRET,
+//             { expiresIn: "1m" }
+//         );
+//         await user.save();
+//         return token;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 //createing model
 const users = new mongoose.model("users", userSchema);
 
 
 module.exports = users;
+
